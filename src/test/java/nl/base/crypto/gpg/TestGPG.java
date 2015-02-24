70c10843cd0cf0e08966172ebaeb46a71b86c3c0
Tim Lamballais Tessensohn <tim@basenet.nl>
2015-02-24 12:53:33 +0100

    Clean up on aisle JUnit. Create util class to ease test creation.

--- src/test/java/nl/base/crypto/gpg/TestGPG.java
+++ src/test/java/nl/base/crypto/gpg/TestGPG.java
@@ -8,13 +8,30 @@ import junit.framework.TestCase;
 
 public class TestGPG extends TestCase {
 
-	public void testImportHaveKey() throws IOException {
-		GPG tool = new GPG(File.createTempFile("JUnit", "pkr"), File.createTempFile("JUnit", "skr"));
+	public void testHaveKey() throws IOException {
+		GPG tool = getNewJUnitGPGTool();
 		try (InputStream is = JUnitUtil.getResourceInputStream("pubkey.asc")) {
-			tool.importKey(is);
+			assertTrue("Key was not successfully imported / haveKey doesn't see it",
+					tool.haveKey(tool.getFingerPrint(is)));
 		}
-		try (InputStream is = JUnitUtil.getResourceInputStream("pubkey.asc")) {
-			assertTrue("Key was not successfully imported.", tool.haveKey(tool.getFingerPrint(is)));
+		assertFalse("Havekey sees a key that it shouldn't.", tool.haveKey("foobar"));
+	}
+
+	/**
+	 *
+	 * Utility method to get a GPG instance with clean keyrings.
+	 *
+	 * 	@return GPG instance with default JUnit keyrings imported
+	 */
+	private GPG getNewJUnitGPGTool() {
+		try (InputStream pkis = JUnitUtil.getResourceInputStream("pubkey.asc");
+				InputStream skis = JUnitUtil.getResourceInputStream("seckey.asc")) {
+			GPG tool = new GPG(File.createTempFile("JUnit", "pkr"), File.createTempFile("JUnit", "skr"));
+			tool.importKey(pkis);
+			tool.importKey(skis);
+			return tool;
+		} catch (IOException e) {
+			throw new RuntimeException(e);
 		}
 	}
 
